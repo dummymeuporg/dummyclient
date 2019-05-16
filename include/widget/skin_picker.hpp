09873aa46527ca0082118be79db68479ef427354
Geoffrey ROYER <geoffrey.royer@gmail.com>
2019-05-16 21:07:58 +0200

    Backup

--- include/widget/skin_picker.hpp
+++ include/widget/skin_picker.hpp
@@ -12,9 +12,15 @@ public:
     virtual bool handleEvent(const sf::Event& event) override;
     SkinPicker& setPos(int, int);
 private:
+    bool _isHovered(const sf::Event&, const sf::Shape&, const sf::Vector2f&&);
+    bool _onMouseButtonPressed(const sf::Event&);
+    bool _onMouseMoved(const sf::Event&);
+
     sf::Vector2f m_origin;
     sf::RectangleShape m_background;
+    sf::Sprite m_sprite;
     sf::CircleShape m_leftBrowser, m_rightBrowser;
+    const sf::Shape* m_hoveredBrowser;
     std::vector<std::string> m_skinList;
 };
 
