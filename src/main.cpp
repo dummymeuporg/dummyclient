fbf2d09ba8f1157df1f530126f2707971e00fbd2
Geoffrey ROYER <geoffrey.royer@gmail.com>
2019-06-07 15:34:58 +0200

    Backup

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,35 @@
 
 #include "config.hpp"
 #include "connector/local_connector.hpp"
+#include "connector/network_connector.hpp"
 #include "local_game_server.hpp"
 #include "game.hpp"
 
 namespace fs = std::filesystem;
+
+int run_standalone(const char* projectPath, const char* serverPath,
+                   const char* account, const char* sessionID)
+{
+    ::LocalGameServer server(projectPath, serverPath);
+    server.run(); // create the account
+    std::shared_ptr<Dummy::Server::GameSession> session =
+        server.buildGameSession();
+    session->start();
+    Connector::LocalConnector connector(*session);
+    ::Config config("dummyclient.ini");
+    ::Game game(account, sessionID, connector, config);
+    return game.run();
+}
+
+int run_remote(const char* host, unsigned short port, const char* account,
+               const char* sessionID)
+{
+    Connector::NetworkConnector connector(host, port);
+    connector.connect();
+    ::Config config("dummyclient.ini");
+    ::Game game(account, sessionID, connector, config);
+    return game.run();
+}
 
 int main(int argc, char* argv[])
 {
@@ -21,23 +46,14 @@ int main(int argc, char* argv[])
             << std::endl;
         exit(EXIT_FAILURE);
     }
-    ::Config config("dummyclient.ini");
 
-    // XXX: dirty.
     std::string connectorType(argv[1]);
     if (connectorType == "standalone") {
-        ::LocalGameServer server(argv[2], argv[3]);
-        server.run(); // create the account
-        std::shared_ptr<Dummy::Server::GameSession> session =
-            server.buildGameSession();
-        session->start();
-        Connector::LocalConnector connector(*session);
-        ::Game game(argv[4], argv[5], connector, config);
-        return game.run();
+        return run_standalone(argv[2], argv[3], argv[4], argv[5]);
+    } else if (connectorType == "remote") {
+        return run_remote(argv[2], atoi(argv[3]), argv[4], argv[5]);
     } else {
-        // XXX: instantiate network connector
-        ::exit(EXIT_FAILURE);
+        return EXIT_FAILURE;
     }
-
 }
 
